0a809d6e1cbf142a07425c8f6cc8b9196667b4ea
bogland <38026094+bogland@users.noreply.github.com>
2021-10-23 23:40:32 +0900

    Update cheatsheet.py

--- cheatsheet.py
+++ cheatsheet.py
@@ -8,16 +8,16 @@ import re
 a = cycle([1,2,3]) # 1 2 3 1 2 3 1 2 3 ...
 list_cycle = [a for a in zip([1,3,1],a)]
 list_cycle.sort(key=lambda x:x[0]) # [(1,x),(1,x),(3,x)]
-dict_groupby = {key: sorted(map(itemgetter(1), value)) for key, value 
+dict_groupby = {key: sorted(map(itemgetter(1), value)) for key, value #map자체론 실행되지 않음에 주의
 in groupby(list_cycle, key=itemgetter(0))}
-print(dict_groupby) # {1: [1, 3], 3: [2]} ,        value : (1, 1), (1, 3) 로 나옴 주의
+dict_groupby # {1: [1, 3], 3: [2]} ,        value : (1, 1), (1, 3) 로 나옴 주의
 
 #DefaultDict
 counts = defaultdict(list)
 attempts = [('dan', 87), ('erik', 95), ('jason', 79), ('erik', 97), ('dan', 100)]
 for (name, score) in attempts:
     counts[name].append(score)
-print(counts)
+counts
 
 #IterTools
 a = repeat(10, 3)  # 10 10 10 ...
@@ -28,7 +28,7 @@ a = zip_longest('AB', 'x', fillvalue='-') # [('A', 'x') ('B', '-')]
 a = [k for k, g in groupby('AAAABBBCCDAABBB')] # A B C D A B
 a = Counter('AAAABBB') # Counter({'A': 4, 'B': 3})
 b = Counter('AB') # Counter({'A': 1, 'B': 1})
-print(a - b, a & b) # - : Counter({'A': 3, 'B': 2}) & : min , | : max
+(a - b, a & b) # - : Counter({'A': 3, 'B': 2}) & : min , | : max
 a = product('ABCD', repeat=2) # [('A', 'A'), ('A', 'B'), ('A', 'C') ... , permutations, combinations
 
 #Heapq
@@ -36,12 +36,13 @@ heap = [] # defaultdict(list)은 안되네
 heapq.heappush(heap, (3,3))
 heapq.heappush(heap, (1,1))
 heapq.heappush(heap, (2,2))
-print(heapq.heappop(heap)) #제일작은 (1,1) pop
+min = heapq.heappop(heap) #제일작은 (1,1) pop
 
 #Regex
 words = re.findall(r'\bf[a-z]*', 'which foot or fell') #['foot', 'fell']
 words = re.findall(r'(\w+)=(\d+)', 'set width=20 and height=10') #[('width','20'),('height','10')]
 words = re.findall(r'\d','1,2,3,4')
+words = re.findall(r'"(.*?)"','"hello"') # ['hello']
 words = re.search(r'[\w]+','123,123') #없으면 None, span(),start(),end()
 
 text = '.+.lemons and limes.-.'
