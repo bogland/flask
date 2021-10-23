d488baca1e2e04a7efb16569d372a02059ea6933
bogland <38026094+bogland@users.noreply.github.com>
2021-10-23 11:51:14 +0900

    Update cheatsheet.py
    수정

--- cheatsheet.py
+++ cheatsheet.py
@@ -1,7 +1,10 @@
 from itertools import *
 from operator import itemgetter
 from collections import defaultdict,Counter
+import heapq
+import re 
 
+#Tuple List to GroupBy
 a = cycle([1,2,3]) # 1 2 3 1 2 3 1 2 3 ...
 list_cycle = [a for a in zip([1,3,1],a)]
 list_cycle.sort(key=lambda x:x[0]) # [(1,x),(1,x),(3,x)]
@@ -9,12 +12,14 @@ dict_groupby = {key: sorted(map(itemgetter(1), value)) for key, value
 in groupby(list_cycle, key=itemgetter(0))}
 print(dict_groupby) # {1: [1, 3], 3: [2]} ,        value : (1, 1), (1, 3) 로 나옴 주의
 
+#DefaultDict
 counts = defaultdict(list)
 attempts = [('dan', 87), ('erik', 95), ('jason', 79), ('erik', 97), ('dan', 100)]
 for (name, score) in attempts:
     counts[name].append(score)
 print(counts)
 
+#IterTools
 a = repeat(10, 3)  # 10 10 10 ...
 a = chain('ABC', 'DEF') # [A B C D E F]
 a = chain([1,2],[3,4]) # [1 2 3 4] 
@@ -25,3 +30,22 @@ a = Counter('AAAABBB') # Counter({'A': 4, 'B': 3})
 b = Counter('AB') # Counter({'A': 1, 'B': 1})
 print(a - b, a & b) # - : Counter({'A': 3, 'B': 2}) & : min , | : max
 a = product('ABCD', repeat=2) # [('A', 'A'), ('A', 'B'), ('A', 'C') ... , permutations, combinations
+
+#Heapq
+heap = [] # defaultdict(list)은 안되네
+heapq.heappush(heap, (3,3))
+heapq.heappush(heap, (1,1))
+heapq.heappush(heap, (2,2))
+print(heapq.heappop(heap)) #제일작은 (1,1) pop
+
+#Regex
+words = re.findall(r'\bf[a-z]*', 'which foot or fell') #['foot', 'fell']
+words = re.findall(r'(\w+)=(\d+)', 'set width=20 and height=10') #[('width','20'),('height','10')]
+words = re.findall(r'\d','1,2,3,4')
+words = re.search(r'[\w]+','123,123') #없으면 None, span(),start(),end()
+
+text = '.+.lemons and limes.-.'
+word = text.strip('.+')  # 양끝에서 벗겨냄, lemons and limes.-
+word = text.find("o")  # 6 
+word = "-".join(["a", "b", "c"]) #a-b-c
+state = "l" in text # True
